fa4d382f72af2f78077d47d817ba53d254f91ddd
christof <christof@cvs.manuproc.berlios.de>
2005-07-21 08:58:11 +0000

    avoid UTF8 if told so

--- src/TeX.h
+++ src/TeX.h
@@ -16,7 +16,7 @@
  *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-// $Id: TeX.h,v 1.7 2005/07/21 08:57:17 christof Exp $
+// $Id: TeX.h,v 1.8 2005/07/21 08:58:11 christof Exp $
 
 #include <iostream>
 #include <string>
@@ -69,7 +69,7 @@ namespace TeX
 	
 	std::ostream &Header(std::ostream &os, HeaderFlags fl=HeaderFlags());
 	std::ostream &Footer(std::ostream &os);
-	static bool TeX_uses_UTF8;
+	extern bool TeX_uses_UTF8;
 	std::string string2TeX(const std::string &s, const StringFlags &fl=StringFlags()) throw();
    std::string scale(const std::string& is, unsigned int maxlength, 
                      const std::string& scale,const bool use_string2TeX=true);
