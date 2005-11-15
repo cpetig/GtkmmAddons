00426e436a6917bf49a2ea9371c39e86d1125795
christof <christof@petig-baender.de>
2005-11-15 14:12:43 +0000

    skip first column of a simpletree (untested)

--- src/Gtk2TeX.cc
+++ src/Gtk2TeX.cc
@@ -38,7 +38,7 @@ static Glib::ustring get_text(const Gtk::TreeModel::const_iterator &y,const Gtk:
 
 static void TreeView2Table_sub(std::ostream &os,const Gtk::TreeView *cl,
 	const Gtk2TeX::TableFlags &fl,const Gtk::TreeModel::const_iterator &y,
-	bool is_last_line)
+	bool is_last_line,int skip_first)
 {  bool has_lines(false),has_text(false);
    int col(1);
    if (fl.preline_cb) (*fl.preline_cb)(os,y,fl.user_data);
@@ -53,7 +53,7 @@ static void TreeView2Table_sub(std::ostream &os,const Gtk::TreeView *cl,
       if (!content.empty())
       {  if (is_lines(content))
          {  has_lines=true;
-            os << "\\cline{"<<col<<'-'<<col<<'}';
+            os << "\\cline{"<<(col-skip_first)<<'-'<<(col-skip_first)<<'}';
          }
          else if (!has_text && !content.empty()) has_text=true;
       }
@@ -65,6 +65,7 @@ static void TreeView2Table_sub(std::ostream &os,const Gtk::TreeView *cl,
       for (Glib::ListHandle<Gtk::TreeViewColumn*>::const_iterator x=cols.begin(),xend=cols.end(),next=x;x!=xend;x=next,x2=next2)
       {  guint multicol=1;
          
+         if (skip_first && !x2) continue;
          for (++next,++next2;next!=xend;++next,++next2)
          {  if (fl.multicolumn && get_text(y,*next).empty()
             	&& (*next)->get_alignment()<0.9)
@@ -76,7 +77,7 @@ static void TreeView2Table_sub(std::ostream &os,const Gtk::TreeView *cl,
          {  os << "\\multicolumn{"<< multicol<< "}{l}{";
          }
          if (fl.element_cb)
-            os << (*fl.element_cb)(y,x2,Gtk2TeX::string2TeX(content),fl.user_data);
+            os << (*fl.element_cb)(y,x2-skip_first,Gtk2TeX::string2TeX(content),fl.user_data);
          else 
             os << Gtk2TeX::string2TeX(content);
          if (multicol>1) os << '}';
@@ -95,14 +96,20 @@ static void TreeView2Table_sub(std::ostream &os,const Gtk::TreeView *cl,
    if (visible_children)
       for (Gtk::TreeModel::Children::const_iterator i=y->children().begin();
    		i!=y->children().end();++i)
-         TreeView2Table_sub(os,cl,fl,i,is_last_line?(i+1==y->children().end()):false);
+         TreeView2Table_sub(os,cl,fl,i,is_last_line?(i+1==y->children().end()):false,skip_first);
 }
 
 std::ostream &Gtk2TeX::TreeView2Table(std::ostream &os,const Gtk::TreeView *cl,const TableFlags &fl)
 {  // gtkmm2 seems to have problems with const pointers so we trick
    Glib::ListHandle<Gtk::TreeViewColumn*> cols=const_cast<Gtk::TreeView*>(cl)->get_columns();
    int columns(cols.size());
+   int skip_first=0;
 
+   if (columns>1 && cl->get_column(0)->get_title()=="tree"
+      && const_cast<Gtk::TreeViewColumn*>(cl->get_column(0))->get_sizing()==Gtk::TREE_VIEW_COLUMN_FIXED
+      && cl->get_column(0)->get_fixed_width()==1)
+     skip_first=1;
+   
    if (fl.environment)
    {  os << "\\begin{" << (fl.longtable?"longtable":"tabular") << "}"
    	"{";
@@ -112,10 +119,11 @@ std::ostream &Gtk2TeX::TreeView2Table(std::ostream &os,const Gtk::TreeView *cl,c
          if ((*i)->get_alignment()>0.66) typ="r";
          else if ((*i)->get_alignment()>0.33) typ="c";
          // p{} ??? GTK_JUSTIFY_FILL ???
+         if (skip_first && !idx) continue;
 
          if (fl.columntype_cb) 
          {  const std::string title((*i)->get_title());
-            typ=(*fl.columntype_cb)(idx,typ,title,fl.user_data);
+            typ=(*fl.columntype_cb)(idx-skip_first,typ,title,fl.user_data);
          }
          os << typ;
       }
@@ -125,26 +133,16 @@ std::ostream &Gtk2TeX::TreeView2Table(std::ostream &os,const Gtk::TreeView *cl,c
    if (fl.headline)
    {  unsigned idx=0;
       for (Glib::ListHandle<Gtk::TreeViewColumn*>::const_iterator i=cols.begin();i!=cols.end();++i,++idx)
-      {  if (!fl.columntitle_cb)
+      {  if (skip_first && !idx) continue;
+         if (!fl.columntitle_cb)
 	    os << string2TeX((*i)->get_title());
 	 else
-	    os << (*fl.columntitle_cb)(idx,(*i)->get_title(),fl.user_data);
+	    os << (*fl.columntitle_cb)(idx-skip_first,(*i)->get_title(),fl.user_data);
          if (idx+1<columns) os << '&';
       }
       if (fl.posthead_cb) (*fl.posthead_cb)(os,fl.user_data);
       os << "\\endhead\n";
    }
-
-#if 0   
-   else if ((fl.first_line!=0 || fl.last_line!=-1) 
-   	&& cl->rows().begin()!=cl->rows().end())  // empty() is non const :-(
-   {  guint last=fl.last_line;
-      if (fl.last_line==-1 || last>cl->rows().size()-1) 
-         last=cl->rows().size()-1;
-      for (guint y=fl.first_line;y<=last;++y)
-      	 CList2Table_sub(os,cl,fl,cl->row(y),y==last);
-   }
-#endif   
 
   if(fl.firstrow_cb)
     {  
@@ -157,7 +155,7 @@ std::ostream &Gtk2TeX::TreeView2Table(std::ostream &os,const Gtk::TreeView *cl,c
    { for (std::vector<Gtk::TreeModel::Path>::const_iterator i=fl.selection.begin();i!=fl.selection.end();++i)
      { std::vector<Gtk::TreeModel::Path>::const_iterator j=i;
        ++j;
-       TreeView2Table_sub(os,cl,fl,const_cast<Gtk::TreeView*>(cl)->get_model()->get_iter(*i),j==fl.selection.end());
+       TreeView2Table_sub(os,cl,fl,const_cast<Gtk::TreeView*>(cl)->get_model()->get_iter(*i),j==fl.selection.end(),skip_first);
      }
    }
    else
@@ -165,7 +163,7 @@ std::ostream &Gtk2TeX::TreeView2Table(std::ostream &os,const Gtk::TreeView *cl,c
       // no working const_iterator yet ...   
       for (Gtk::TreeModel::iterator y=const_cast<Gtk::TreeView*>(cl)->get_model()->children().begin();
 			y!=yend;++y)
-         TreeView2Table_sub(os,cl,fl,y,y+1==yend);
+         TreeView2Table_sub(os,cl,fl,y,y+1==yend,skip_first);
    }
    
    if (fl.postlist_cb) (*fl.postlist_cb)(os,fl.user_data);
