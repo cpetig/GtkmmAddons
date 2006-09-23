1121872030c3aa7ad82c04aec7198fc6130b342e
christof <christof@petig-baender.de>
2006-09-23 15:03:50 +0000

    search should start now

--- src/SearchCombo2.cc
+++ src/SearchCombo2.cc
@@ -1,21 +1,25 @@
 #include "SearchCombo2.h"
 #include <gtkmm/liststore.h>
+#include <cassert>
 
 struct Gtk::SearchCombo2::Model : Gtk::ListStore
 //virtual Glib::Object, Gtk::TreeModel
 { 
 private:
-  Gtk::Entry *entry;
-  Model(const TreeModelColumnRecord& columns, Gtk::Entry *e);
+  Gtk::ComboBoxEntry *combo;
+  Model(const TreeModelColumnRecord& columns, Gtk::ComboBoxEntry *e);
 //  std::vector<std::string> results;
 
-  void idle_fun();
+  bool fill_idle();
+  void fill_list();
+  bool close_search();
+  void clear_list();
 public:
   sigc::signal2<void,gboolean *,GtkSCContext> search;
   
-  static Glib::RefPtr<Model> create(const TreeModelColumnRecord& columns, Gtk::Entry *e);
+  static Glib::RefPtr<Model> create(const TreeModelColumnRecord& columns, Gtk::ComboBoxEntry *e);
   
-  void trigger_search();
+  void entry_changed();
   void stop_if_running();
 
   bool start_idle:1;	/** start search at idle time not in focus_in */
@@ -57,8 +61,8 @@ private:
    virtual Path get_path_vfunc(const TreeModel::iterator& iter) const; */
 };
 
-Gtk::SearchCombo2::Model::Model(const TreeModelColumnRecord& columns, Gtk::Entry *e)
-  : ListStore(columns), entry(e),
+Gtk::SearchCombo2::Model::Model(const TreeModelColumnRecord& columns, Gtk::ComboBoxEntry *e)
+  : ListStore(columns), combo(e),
     start_idle(true), backspace(), search_in_progress(), already_started(),
     search_finished(), value_selected(), reopen()
 //  : Glib::ObjectBase( typeid(Model) ), //register a custom GType.
@@ -66,13 +70,60 @@ Gtk::SearchCombo2::Model::Model(const TreeModelColumnRecord& columns, Gtk::Entry
 {
 }
 
-void Gtk::SearchCombo2::Model::trigger_search()
+void Gtk::SearchCombo2::Model::clear_list()
 { stop_if_running();
-  
+  Gtk::ListStore::clear();
+  combo->popdown();
+  search_finished=false;
+  value_selected=false;
 }
 
-void Gtk::SearchCombo2::Model::idle_fun()
-{
+void Gtk::SearchCombo2::Model::fill_list()
+{ gboolean continue_=false;
+  reopen=value_selected;
+  clear_list();
+  assert(!search_in_progress);
+  search_in_progress=true;
+  already_started=false;
+  value_selected=false;
+/*  if (!start_idle)
+  {
+  } else */
+    continue_=true;
+  if (continue_)
+  { idle_conn=Glib::signal_idle().connect(sigc::mem_fun(*this,&Model::fill_idle));
+  }
+  else close_search();
+  combo->popup();
+}
+
+void Gtk::SearchCombo2::Model::entry_changed()
+{ // narrow optimization (gtksearchcombo.c:712)
+  fill_list();
+}
+
+bool Gtk::SearchCombo2::Model::close_search()
+{ search_in_progress=false;
+  search_finished=true;
+  idle_conn.disconnect();
+  search(0, GTK_SEARCH_CLOSE);
+  /* if (autoexpand && !backspace)
+  { 
+  } */
+  return false;
+}
+
+bool Gtk::SearchCombo2::Model::fill_idle()
+{ gboolean continue_=false;
+  // THREADS_ENTER?
+  assert(search_in_progress && !search_finished);
+  search(&continue_, already_started?GTK_SEARCH_FETCH:
+    (reopen?GTK_SEARCH_REOPEN:GTK_SEARCH_OPEN));
+  already_started=true;
+  if (!continue_)
+  { close_search();
+  }
+  return continue_;
 }
 
 void Gtk::SearchCombo2::Model::stop_if_running()
@@ -84,16 +135,16 @@ void Gtk::SearchCombo2::Model::stop_if_running()
   }
 }
 
-Glib::RefPtr<Gtk::SearchCombo2::Model> Gtk::SearchCombo2::Model::create(const TreeModelColumnRecord& columns, Gtk::Entry *e)
+Glib::RefPtr<Gtk::SearchCombo2::Model> Gtk::SearchCombo2::Model::create(const TreeModelColumnRecord& columns, Gtk::ComboBoxEntry *e)
 { return Glib::RefPtr<Model>(new Model(columns,e));
 }
 
 void Gtk::SearchCombo2::on_entry_changed()
-{ mymodel->trigger_search();
+{ mymodel->entry_changed();
 }
 
 Gtk::SearchCombo2::SearchCombo2(bool,bool)
-{ mymodel=Model::create(m_text_columns,get_entry());
+{ mymodel=Model::create(m_text_columns,this);
   get_entry()->signal_changed().connect(sigc::mem_fun(*this,&SearchCombo2::on_entry_changed));
 }
 
