c9597351d2d6698f6893dc4c9fe62954b4c312fa
petig <petig@stahlwille.de>
2011-06-30 09:56:23 +0000

    ja_nein_dialog hinzugefügt

---
+++ src/ja_nein_frage.hh
@@ -0,0 +1,18 @@
+// generated 2011/6/30 11:53:48 CEST by christof@christof-hp.(none)
+// using glademm V2.13.0
+//
+// newer (non customized) versions of this file go to ja_nein_frage.hh_new
+
+// you might replace
+//    class foo : public foo_glade { ... };
+// by
+//    typedef foo_glade foo;
+// if you didn't make any modifications to the widget
+
+#ifndef _JA_NEIN_FRAGE_HH
+#  include "ja_nein_frage_glade.hh"
+#  define _JA_NEIN_FRAGE_HH
+class ja_nein_frage : public ja_nein_frage_glade
+{  
+};
+#endif
